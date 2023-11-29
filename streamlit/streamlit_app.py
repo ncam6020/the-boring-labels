71cd6bafdf7782df474a8c378f9dd58037f500a9
nick cameron <nicholas.cameron@perkinswill.com>
2023-11-29 16:50:58 -0600

    with json training save

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,8 +1,15 @@
 ##############################env\Scripts\Activate.ps1#######################
+##############################env\Scripts\Activate.ps1#######################
 import pandas as pd
 import streamlit as st
 import torch
+import os
+import json
 from sentence_transformers import SentenceTransformer, util
+
+# Get the directory of the current script
+base_dir = os.path.dirname(__file__)
+
 
 # Set Streamlit page configuration
 st.set_page_config(layout="centered", page_title="The CSV Boring Machine", page_icon="🐗")
@@ -33,8 +40,11 @@ with st.sidebar:
     uploaded_classifier_file = st.file_uploader("Upload CSV with classifier/boring list", type=["csv"])
     uploaded_room_names_file = st.file_uploader("Upload CSV with original room names", type=["csv"])
 
-# Main logic for processing files
-if uploaded_classifier_file and uploaded_room_names_file:
+# Initialize an empty DataFrame for final_csv_df
+final_csv_df = pd.DataFrame()
+
+# Check if files are uploaded
+if uploaded_classifier_file is not None and uploaded_room_names_file is not None:
     classifier_df = pd.read_csv(uploaded_classifier_file)
     room_names_df = pd.read_csv(uploaded_room_names_file)
 
@@ -50,13 +60,13 @@ if uploaded_classifier_file and uploaded_room_names_file:
         unique_count = row['Unique Count']
         top_match_indices, top_match_scores = calculate_similarities(original_name, boring_names_embeddings)
         
-        # Append data with the top match score for web app display
+        # Append data with the top match score
         annotations_data.append({
             "Original Room Name": original_name,
             "Unique Count": unique_count,
             "Selected Boring Name": f"{boring_names[top_match_indices[0]]} -{top_match_scores[0]*100:.0f}%",
             "Top Boring Name Suggestion": boring_names[top_match_indices[0]],
-            "Top Match Score": top_match_scores[0],  # Decimal format score
+            "Top Match Score": top_match_scores[0],
             "Boring Name Options": sorted(boring_names)
         })
 
@@ -83,23 +93,61 @@ if uploaded_classifier_file and uploaded_room_names_file:
         if edited_df is not None:
             df['Selected Boring Name'] = edited_df['Selected Boring Name']
 
-        # Adjust final DataFrame for CSV download
+        # Merge and create final DataFrame
         final_df = room_names_df.merge(
             df[['Original Room Name', 'Selected Boring Name', 'Top Boring Name Suggestion', 'Top Match Score']],
             on='Original Room Name',
             how='left'
         )
+
         # Removing probability score from 'Selected Boring Name' in final CSV
         final_df['Selected Boring Name'] = final_df['Selected Boring Name'].str.split(' -').str[0]
 
-        # Prepare final CSV for download
+        # Update final_csv_df
         final_csv_df = final_df[['Original Room Name', 'Selected Boring Name', 'Top Boring Name Suggestion', 'Top Match Score']]
 
-        st.download_button(
-            "⬇️ Download annotations as .csv",
+        # Streamlit download button and appending logic
+        downloaded = st.download_button(
+            "⬇️ Download Your Boring Name File",
             final_csv_df.to_csv(index=False),
             "annotated_room_names.csv",
             mime='text/csv',
             use_container_width=True
         )
+
+        # File path for the existing JSON file
+        json_file_path = os.path.join(base_dir, "trainingdata", "training.json")
+
+        # Append data when download button is clicked
+        if downloaded:
+            if not final_csv_df.empty:
+                try:
+                    # Convert DataFrame to JSON
+                    data_json = final_csv_df.to_json(orient='records')
+
+                    # Check if JSON file exists
+                    if os.path.exists(json_file_path):
+                        with open(json_file_path, 'r+') as file:
+                            # Read existing data into a list
+                            file_data = json.load(file)
+                            # Check if file_data is a list and append new data
+                            if isinstance(file_data, list):
+                                file_data.extend(json.loads(data_json))
+                                # Set file's current position at offset
+                                file.seek(0)
+                                file.truncate()
+                                # Write updated data
+                                json.dump(file_data, file, indent=4)
+                            else:
+                                raise ValueError("JSON file does not contain a list")
+                    else:
+                        # Create new JSON file and write data
+                        with open(json_file_path, 'w') as file:
+                            json.dump(json.loads(data_json), file, indent=4)
+                    
+                    st.success("You Have Now Contributed to the Boring AI Model -thanks!")
+                except Exception as e:
+                    st.error(f"An error occurred: {e}")
+            else:
+                st.warning("The DataFrame to be appended is empty.")
 
